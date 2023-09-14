03d4f6fe7b47f5839c6782b37a16e13c3f38fa6e
cc299792458 <cc2018011855@gmail.com>
2023-09-14 12:43:36 -0700

    adapt base_env of assembly

--- mani_skill2/envs/assembly/base_env.py
+++ mani_skill2/envs/assembly/base_env.py
@@ -8,6 +8,7 @@ from mani_skill2.agents.base_agent import BaseAgent
 from mani_skill2.agents.configs.panda.defaults import PandaRealSensed435Config
 from mani_skill2.agents.robots.panda import Panda
 from mani_skill2.agents.robots.xmate3 import Xmate3Robotiq
+from mani_skill2.agents.robots.xarm import XArm7, XArm7D435
 from mani_skill2.envs.sapien_env import BaseEnv
 from mani_skill2.sensors.camera import CameraConfig
 from mani_skill2.utils.sapien_utils import (
@@ -19,10 +20,10 @@ from mani_skill2.utils.sapien_utils import (
 
 
 class StationaryManipulationEnv(BaseEnv):
-    SUPPORTED_ROBOTS = {"panda": Panda, "xmate3_robotiq": Xmate3Robotiq}
+    SUPPORTED_ROBOTS = {"panda": Panda, "xmate3_robotiq": Xmate3Robotiq, "xarm7": XArm7, "xarm7_d435": XArm7D435}
     agent: Union[Panda, Xmate3Robotiq]
 
-    def __init__(self, *args, robot="panda", robot_init_qpos_noise=0.02, **kwargs):
+    def __init__(self, *args, robot="xarm7_d435", robot_init_qpos_noise=0.0, **kwargs):
         self.robot_uid = robot
         self.robot_init_qpos_noise = robot_init_qpos_noise
         super().__init__(*args, **kwargs)
@@ -32,17 +33,17 @@ class StationaryManipulationEnv(BaseEnv):
         scene_config.enable_pcm = True
         return scene_config
 
-    def _configure_agent(self):
+    def _configure_agent(self, sim_params, ee_type):
         agent_cls: Type[BaseAgent] = self.SUPPORTED_ROBOTS[self.robot_uid]
         if self.robot_uid == "panda":
             self._agent_cfg = PandaRealSensed435Config()
         else:
-            self._agent_cfg = agent_cls.get_default_config()
+            self._agent_cfg = agent_cls.get_default_config(sim_params, ee_type)
 
-    def _load_agent(self):
-        agent_cls: Type[Panda] = self.SUPPORTED_ROBOTS[self.robot_uid]
+    def _load_agent(self, sim_params, ee_type):
+        agent_cls: Type[XArm7] = self.SUPPORTED_ROBOTS[self.robot_uid]
         self.agent = agent_cls(
-            self._scene, self._control_freq, self._control_mode, config=self._agent_cfg
+            self._scene, self._control_freq, self._control_mode, config=self._agent_cfg, sim_params=sim_params, ee_type=ee_type,
         )
         self.tcp: sapien.Link = get_entity_by_name(
             self.agent.robot.get_links(), self.agent.config.ee_link_name
@@ -71,6 +72,24 @@ class StationaryManipulationEnv(BaseEnv):
             )
             self.agent.reset(qpos)
             self.agent.robot.set_pose(Pose([-0.562, 0, 0]))
+        elif self.robot_uid in ['xarm7', 'xarm7_d435']:
+            # TODO: verify the initial pose of ee
+            if self.ee_type == 'reduced_gripper':
+                qpos = np.array(
+                    [0, 0, 0, np.pi / 3, 0, np.pi / 3, -np.pi / 2, 0.0446430, 0.0446430]
+                )
+                qpos[:-2] += self._episode_rng.normal(
+                    0, self.robot_init_qpos_noise, len(qpos) - 2
+                )
+            elif self.ee_type == 'full_gripper':
+                qpos = np.array(
+                    [0, 0, 0, np.pi / 3, 0, np.pi / 3, -np.pi / 2, 0, 0, 0, 0, 0, 0]
+                )
+                qpos[:-6] += self._episode_rng.normal(
+                    0, self.robot_init_qpos_noise, len(qpos) - 6
+                )
+            self.agent.reset(qpos)
+            self.agent.robot.set_pose(Pose([-0.463, 0.0, 0.0]))
         else:
             raise NotImplementedError(self.robot_uid)
 
