3ad8c78d1e3d8bf8ba7a713642d8ab11c8041a99
cc299792458 <cc2018011855@gmail.com>
2023-11-05 17:29:36 -0800

    add xarm to peginsertionside

--- mani_skill2/envs/assembly/peg_insertion_side.py
+++ mani_skill2/envs/assembly/peg_insertion_side.py
@@ -124,6 +124,15 @@ class PegInsertionSideEnv(StationaryManipulationEnv):
             )
             self.agent.reset(qpos)
             self.agent.robot.set_pose(Pose([-0.615, 0, 0]))
+        elif self.robot_uid in ['xarm7', 'xarm7_d435']:
+            qpos = np.array(
+                    [0, 0, 0, np.pi / 3, 0, np.pi / 3, -np.pi / 2, 0.0446430, 0.0446430]
+                )
+            qpos[:-2] += self._episode_rng.normal(
+                0, self.robot_init_qpos_noise, len(qpos) - 2
+            )
+            self.agent.reset(qpos)
+            self.agent.robot.set_pose(Pose([-0.4639, 0.0, 0.0]))
         else:
             raise NotImplementedError(self.robot_uid)
 
