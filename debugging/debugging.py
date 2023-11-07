e3f53411d67b11fd683b7a8d1d8b4b881f421632
cc299792458 <cc2018011855@gmail.com>
2023-11-07 10:39:36 -0800

    debugging peginsertionside v4

--- debugging/debugging.py
+++ debugging/debugging.py
@@ -4,7 +4,7 @@ import numpy as np
 import mani_skill2.envs 
 
 
-env = gym.make(id="PegInsertionSide-v1",
+env = gym.make(id="PegInsertionSide-v4",
                control_mode='pd_ee_vel_pos',)
 obs = env.reset()
 env.step(np.array([0.0, 0.0, 0.0, 0.0]))
